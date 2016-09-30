97de5a788478b42029cf887c0bbe7a8f1478de96
Santiago Basulto <santiago.basulto@gmail.com>
2016-09-30 11:25:02 -0300

    Improved test runner

--- README.md
+++ README.md
@@ -13,6 +13,21 @@ This course will take you through the fundamentals of programming building from 
 * You get stuck easily and you don't know how to get unstuck, how to debug your error or find help.
 * You need other people to surround you and encourage you to learn so you don't end up procrastinating.
 
+## Development
+
+To run all the tests for the assignment lesson use the `run_tests.py` script. Examples:
+
+```bash
+$ python run_tests.py  # Run all tests
+$ python run_tests.py  -v # Run all tests including logging info
+$ python run_tests.py -u 1  # Run tests just for the unit 1
+$ python run_tests.py -u 5 8  # Run tests just for units 5 and 8
+$ python run_tests.py -g string  # Run tests for lessons that contain the
+                                 # keyword 'string' in their name
+$ python run_tests.py -g string length  # Run tests for lessons that contain
+                                        # the keywords 'string' and 'length' in their name
+```
+
 ## License
 
 This content is released under the [CC BY-SA 4.0](http://creativecommons.org/licenses/by-sa/4.0/) Creative Commons license.
--- run_tests.py
+++ run_tests.py
@@ -2,6 +2,36 @@ import unittest
 import os
 import sys
 
+import logging
+import logging.config
+d = {
+    'version': 1,
+    'formatters': {
+        'detailed': {
+            'class': 'logging.Formatter',
+            'format': '%(asctime)s %(name)-15s %(levelname)-8s %(processName)-10s %(message)s'
+        },
+        'colored': {
+            '()': 'colorlog.ColoredFormatter',
+            'format': "%(log_color)s%(levelname)-6s%(reset)s %(message)s %(blue)s\"%(lesson_name)s\""
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'INFO',
+            'formatter': 'colored',
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'INFO'
+        }
+    }
+}
+logging.config.dictConfig(d)
+logger = logging.getLogger(__name__)
 try:
     from tempfile import TemporaryDirectory
 except ImportError:
@@ -35,9 +65,11 @@ class InvalidLessonException(Exception):
 
 
 class Lesson(object):
-    def __init__(self, uuid, path):
+    def __init__(self, name, uuid, path, _lesson_data):
+        self.name = name
         self.uuid = uuid
         self.path = path
+        self._lesson_data = _lesson_data
         self.__test_data = None
 
     @property
@@ -59,12 +91,21 @@ def read_lesson_tests(lesson_path):
     with tests_path.open('r') as tests_f:
         return tests_f.read()
 
+def _get_unit_number(unit_name):
+    start = unit_name.index('-') + 1
+    end = unit_name.index('-', start)
+    return int(unit_name[start:end])
 
-def iter_code_lessons(path='.', unit_glob='unit-*',
+
+def iter_code_lessons(path='.', unit_glob='unit-*', grep=None, units=None,
                       lesson_glob='lesson-*', rmotr_toml_name='.rmotr'):
-    p = Path(path)
+    grep = grep or []
 
+    p = Path(path)
     for unit in p.glob(unit_glob):
+        unit_number = _get_unit_number(unit.name)
+        if units and unit_number not in units:
+            continue
         for lesson_path in unit.glob(lesson_glob):
             rmotr_toml = lesson_path / rmotr_toml_name
             if not rmotr_toml.exists():
@@ -74,9 +115,20 @@ def iter_code_lessons(path='.', unit_glob='unit-*',
 
             with rmotr_toml.open() as rmotr_f:
                 lesson_data = toml.load(rmotr_f)
-                if lesson_data['type']:
-                    yield Lesson(uuid=lesson_data['uuid'],
-                                 path=lesson_path)
+                lesson_name = lesson_data['name']
+                if lesson_data['type'] == 'assignment':
+                    add_lesson = True
+                    for keyword in grep:
+                        if keyword not in lesson_name.lower():
+                            add_lesson = False
+                            break
+                    if not add_lesson:
+                        continue
+                    logger.info("Added lesson", extra={'lesson_name': lesson_name})
+                    yield Lesson(name=lesson_name,
+                                 uuid=lesson_data['uuid'],
+                                 path=lesson_path,
+                                 _lesson_data=lesson_data)
 
 
 def iter_lesson_solutions(lesson):
@@ -95,15 +147,16 @@ def write_solution_test_file(_dir, solution):
         test_f.write(solution.lesson.test_data)
 
 
-def test_lessons_solutions(exit=True):
+def test_lessons_solutions(grep=None, units=None, exit=True):
+
     with TemporaryDirectory() as temp_dir:
         abs_path = os.path.abspath(temp_dir)
-        for lesson in iter_code_lessons():
+        for lesson in iter_code_lessons(grep=grep, units=units):
             for solution in iter_lesson_solutions(lesson):
                 write_solution_test_file(temp_dir, solution)
 
         suite = unittest.TestLoader().discover(abs_path)
-        program = TestProgram(suite=suite, exit=False)
+        program = TestProgram(suite=suite, exit=False, argv=sys.argv[0:1])
 
         if exit:
             sys.exit(int(not program.success))
@@ -111,7 +164,15 @@ def test_lessons_solutions(exit=True):
         return program
 
 
-main = test_lessons_solutions
-
 if __name__ == '__main__':
-    main()
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Run tests')
+    parser.add_argument('-g', '--grep', nargs='+', required=False)
+    parser.add_argument('-u', '--units', nargs='+', type=int, required=False)
+    parser.add_argument('-v', '--verbose', action='store_true', required=False)
+    args = parser.parse_args()
+    if not args.verbose:
+        logger.setLevel('ERROR')
+
+    test_lessons_solutions(grep=args.grep, units=args.units)
