91c6a98e8eb98cd1b74e51e33071796931087541
nikihapy <nikihapy@gmail.com>
2016-10-21 16:16:55 -0700

    Update main.py

--- unit-3-control-flow/lesson-2-conditional-multiplication/main.py
+++ unit-3-control-flow/lesson-2-conditional-multiplication/main.py
@@ -3,3 +3,8 @@ def conditional_multiplication(a_condition, number):
         return number*10
     else: 
         return number
+
+result=conditional_multiplication(True,20)
+print result
+result=conditional_multiplication(False,5)
+print result
